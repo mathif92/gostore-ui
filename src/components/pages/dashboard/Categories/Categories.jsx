ff907fec7c16a23c1faf0cb0afd8ede2500e6ef5
mathif92 <mathif925@gmail.com>
2016-11-26 19:11:47 -0300

    Changes :     +   API Rest call added, still it does not work yet!

--- src/components/pages/dashboard/Categories/Categories.jsx
+++ src/components/pages/dashboard/Categories/Categories.jsx
@@ -1,28 +1,73 @@
-import React, { PropTypes, Component } from 'react';
-import { Link } from "react-router";
+import React, {PropTypes, Component} from 'react';
+import {Link} from "react-router";
 import {Jumbotron} from 'react-bootstrap';
+import $ from "jquery";
+import {Button, Icon, Image as ImageComponent, Dimmer, Loader, Item, Label} from 'semantic-ui-react'
+import history from "../../../../app";
+
 
 var Blank = React.createClass({
 
-  categories: function() {
+    constructor() {
+        this.state = {categories: []};
+    },
 
-  },
+    componentDidMount() {
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/categories',
+            data: data
+        }).done(function (result) {
+            this.setState({categories: result.json()});
+            console.log('Categories : ' + result.json());
+        }).fail(function (jqXHR, status) {
+            console.log('failed to load the categories');
+            return false;
+        });
+    },
 
-  render: function() {
-    return (
-      <div className="overview-page" key="overview"> 
-        <Link to="/dashboard/reports" className="pull-right btn btn-primary btn-outline btn-rounded">Reports</Link> 
-        <h2>Categories <small>Select a category to list its products</small></h2>
-        <Jumbotron> 
-          <h1>Welcome!</h1> I don't want to bloat this Ani Theme Free Edition AngularJS Admin theme with a lot of plugins. To install plugins we have bower. Let's keep it real clean!
-          <br /><br /> 
-          <p> <a className="btn btn-primary btn-lg btn-outline btn-rounded">Learn more</a> </p> 
-        </Jumbotron> 
-      </div>
-      
-      
-    );
-  }
+    render: function () {
+        var categoryList = this.state.categories.map(function (category) {
+            return (
+                <Item>
+                    <Item.Image src={category.imageURL}/>
+
+                    <Item.Content>
+                        <Item.Header as='a'>{category.name}</Item.Header>
+                        <Item.Meta>
+                            <span className='cinema'>IFC Cinema</span>
+                        </Item.Meta>
+                        <Item.Description>{category.description}</Item.Description>
+                        <Item.Extra>
+                            <Button primary floated='right' onClick={this.handleCategorySelection}>
+                                Ver Productos
+                                <Icon name='right chevron'/>
+                            </Button>
+                            {/*<Label>Limited</Label>*/}
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
+            );
+        });
+        return (
+            <div className="overview-page" key="overview">
+                <Link to="/dashboard/reports"
+                      className="pull-right btn btn-primary btn-outline btn-rounded">Reports</Link>
+                <h2>Categorías
+                    <small>Selecciona "Ver Productos" para ver los productos de una categoría</small>
+                </h2>
+                <Jumbotron>
+                    <p><a className="btn btn-primary btn-lg btn-outline btn-rounded">{categoryList}</a></p>
+                </Jumbotron>
+            </div>
+
+
+        );
+    },
+
+    handleCategorySelection: function () {
+
+    }
 
 });
 
